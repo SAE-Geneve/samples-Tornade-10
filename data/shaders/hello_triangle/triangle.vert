b839f81968d9f30481fdaceb0ce7c42cc31f7a42
Tornado <maxence.ribeau@gmail.com>
2024-11-28 15:49:28 +0100

    Start-of-3D
    TODO :
    - Fix Cubes
    - Create Camera
    - Create Light

--- data/shaders/hello_triangle/triangle.vert
+++ data/shaders/hello_triangle/triangle.vert
@@ -1,13 +1,16 @@
 #version 300 es
 precision highp float;
 
-out vec3 fragColor;
 out vec2 TexCoord;
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 fragColor;
 layout (location = 1) in vec2 aTexCoord;
 
 uniform mat4 transform;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 vec3 colors[3] = vec3[](
 vec3(1.0, 1.0, 1.0),
@@ -44,8 +47,7 @@ vec2(0.0f, 0.0f) // down-left corner
 
 void main()
 {
-    gl_Position = transform * vec4(square[gl_VertexID], 0.0, 1.0);
+    gl_Position = projection * view * model * vec4(square[gl_VertexID], 1.0);
     fragColor = colors[gl_VertexID];
     TexCoord = texCoords[gl_VertexID];
-    // TexCoord = vec2(texCoords.x, texCoords.y);
 }
--- main/hello_triangle.cpp
+++ main/hello_triangle.cpp
@@ -124,7 +124,25 @@ namespace gpr5300
     {
         elapsed_time_ += dt;
 
+        // 3D rotations
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
 
+        // note that we're translating the scene in the reverse direction of where we want to move
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(program_, "model");
+        int viewLoc = glGetUniformLocation(program_, "view");
+
+    	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+
+        
+
+        // 2D rotations
         glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
         glm::mat4 trans = glm::mat4(1.0f);
 
